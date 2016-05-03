73af668f420acd6f6989115eed3943236bc45015
wuzhuobin <317854987@qq.com>
2016-05-03 14:11:17 +0800

    14.11

--- smartfMRI/smartfmri.cpp
+++ smartfMRI/smartfmri.cpp
@@ -27,7 +27,7 @@ int SmartfMRI::removeExperiment()
 	QDir dirr(ui.experimentlistView->
 		currentIndex().data(Qt::ToolTipRole).toString());
 	dirr.setFilter(QDir::NoDotAndDotDot);
-	if (dirr.removeRecursively()) {
+	if (dirr != QDir::current() && dirr.removeRecursively()) {
 		qDebug() << "remove successfully";
 		delete expMod;
 		expMod = new ExperimentModel(this, QDir(dir.path() + "/paradigm"));
