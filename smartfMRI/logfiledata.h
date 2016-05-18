0f9dc875a90f7baf3a967869b6f05d3dc04c50f4
wuzhuobin <317854987@qq.com>
2016-05-18 20:24:16 +0800

    version 3.1

--- smartfMRI/logfiledata.h
+++ smartfMRI/logfiledata.h
@@ -5,10 +5,22 @@
 #include <QFile>
 #include <QDebug>
 #include <QDateTime>
-
+/**
+ * transform function
+ * transform the Log file to a readable format
+ * only on static funtion
+ * @authro
+ * @version
+ */
 class LogFileData : public QObject
 {
 public:
+	/**
+	 * static function
+	 * doing format transformation
+	 * @param fileContent all file content of a log
+	 * @retrun each item of QList is a row QString
+	 */
 	static QList<QString> change(QString fileContent);
 };
 
--- smartfMRI/scanparameters.h
+++ smartfMRI/scanparameters.h
@@ -7,9 +7,17 @@
 #include <QDir>
 #include <QFile>
 
+/**
+ * read Scan Parameters of *.txt files
+ * read & write methods are used to read and write the *.txt files content
+ * attributes and values are null, if read is not invoked
+ * @author
+ * @version
+ */
 class ScanParameters : public QObject
 {
 public:
+	// status of reading or writing *.txt files
 	enum status
 	{
 		CannotOpenTheFile,
@@ -17,17 +25,48 @@ public:
 		Successful,
 		ParametersIncorrect
 	};
+	/**
+	 * constructor
+	 * 
+	 * @param fileName file name of *.txt file
+	 * @param parent parent QObject
+	 */
 	ScanParameters(QString fileName = QString(), QObject *parent = 0);
+	/**
+	 * read the *.txt file content to attributes and values 
+	 * they are null if read is not invoked
+	 * @return  status of reading *.txt files
+	 */
 	status read();
+	/**
+	 * write the *.text file with attributes and values
+	 * read should be invoked first
+	 * @return status of writing *.txt files
+	 */
 	status write();
+	/**
+	 * destructor
+	 */
 	~ScanParameters();
-	
+	/**
+	 * get the reference of attributes
+	 * can change it directly, then use write method for writing it to *.txt files
+	 * @return referenc of attibutes
+	 */
 	QList<QString>& getAttributes();
+	/**
+	 * get the reference of values
+	 * can chage it directly, then use write mothod for writing it to *.txt files
+	 * @return referenc of values
+	 */
 	QList<QString>& getValues();
 
 private:
+	//the file name of *.txt file
 	QString parametersFileName;
+	// the first row of the *.txt file, every attribute should be seperated by '/t'
 	QList<QString> attributes;
+	// the first second of the *.txt file, every value should be seperated by '/t'
 	QList<QString> values;
 };
 
